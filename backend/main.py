79b934922463cf64c22e9581455d120674860074
Wainon <o.ruz.nm@gmail.com>
2026-05-13 22:31:38 +0300

    Обновление приёма изображения

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+
 import json
+import base64
+import cv2
+import numpy as np
 import random
 
 app = FastAPI()
 
-
-# Разрешаем frontend подключаться
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,44 +17,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-# Проверка API
-@app.get("/")
-async def root():
-    return {
-        "message": "PingVision AI backend работает"
-    }
-
-
-# WebSocket endpoint
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
 
     await websocket.accept()
 
-    print("Client connected")
+    print("Client connected", flush=True)
 
     try:
 
         while True:
 
-            # Получаем данные от frontend
-            data = await websocket.receive_text()
+            data_url = await websocket.receive_text()
+            if "," in data_url:
+                base64_str = data_url.split(",")[1]
+            else:
+                base64_str = data_url
 
-            print("Frame received")
+            image_bytes = base64.b64decode(base64_str)
 
-            # Здесь потом будет YOLO/OpenCV
+            nparr = np.frombuffer(image_bytes, np.uint8)
 
-            # Временные координаты мяча
+            frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+            if frame is None:
+                print("Frame decode failed")
+                continue
+
+            print("Frame:", frame.shape, flush=True)
+
             result = {
-                "x": random.randint(100, 500),
+                "x": random.randint(0,1),
                 "y": random.randint(100, 400),
                 "speed": random.randint(20, 80),
                 "confidence": round(random.uniform(0.8, 0.99), 2)
             }
 
-            # Отправляем результат обратно
-            await websocket.send_text(json.dumps(result))
+            await websocket.send_text(json.dumps(result)) 
 
     except Exception as e:
         print("Disconnected:", e)
